97530087465e2a2807bd5a3313f8b6b64c443039
AlexanderPetrik <painkiller408@gmail.com>
2023-06-09 00:53:44 +0300

    in progress

--- src/api/cat-api.js
+++ src/api/cat-api.js
@@ -1,12 +1,37 @@
-const URL = 'https://api.thecatapi.com/v1/images/search';
+const URL = 'https://api.thecatapi.com/v1';
+const headers = {
+    headers: {
+       'x-api-key' : 'api_key=live_tzUAV1WarGXq6gMm0liTGEEZrUB73EAL2S74HlhzvxKlrY4zPEga8ttGvvkkhmxG'
+    }
+  }
+
+export function fetchImageByBreed(breedId) {
+  return fetch(`${URL}/images/search?breed_ids=${breedId}`, headers)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Oops! Something went wrong! Try reloading the page!');
+      }
+      return response.json();
+    });
+}
 
 export function fetchCatByBreed(breedId) {
-  return fetch(
-    `${URL}/${breedId}?fields=name,capital,population,flags,languages`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error('Oops! Something went wrong! Try reloading the page!');
-    }
-    return response.json();
-  });
+  return fetch(`${URL}/breeds/${breedId}`, headers)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Oops! Something went wrong! Try reloading the page!');
+      }
+      return response.json();
+    });
 }
+
+export function fetchBreeds() {
+  return fetch(`${URL}/breeds`, headers)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Oops! Something went wrong! Try reloading the page!');
+      }
+      return response.json();
+    });
+}
+
--- src/index.js
+++ src/index.js
@@ -1,7 +1,61 @@
 import './css/styles.css';
-import { fetchCatByBreed } from './api/fetchCountries';
+import { fetchCatByBreed, fetchImageByBreed, fetchBreeds } from './api/cat-api';
+import Notiflix from 'notiflix';
+
+// Получить рефс +
+// Получить список пород +
+// При загрузке вывести список пород в селект +
+// При выборе из силекта получить полную ин-фу о породе +
+// Вывисти ин-фу на страницу +
+// Очистка +
+// При загрузке показывать загрузку -
+// Добавить обработку ошибок -
+// Добавить оформление стилей на страницу:
+    // селект -
+    // loader -
+    // Error -
 
 const refs = {
   breedCats: document.querySelector('.breed-select'),
-  catInfo: document.querySelector('cat-info'),
+  catInfo: document.querySelector('.cat-info'),
 }
+
+// console.log(refs);
+
+fetchBreeds().then(response => {
+  console.log(response);
+  const options = response.map((cat) => {
+    const option = document.createElement(`option`);
+    option.value = cat.id;
+    option.textContent = cat.name;
+    return option
+  })
+  refs.breedCats.append(...options);
+});
+
+refs.breedCats.addEventListener('change', (event) => {
+  refs.catInfo.innerHTML = '';
+
+  fetchCatByBreed(event.target.value).then(response => {
+    const catName = document.createElement(`h2`);
+    catName.textContent = response.name;
+    const catDescription = document.createElement(`p`);
+    catDescription.textContent = response.description;
+    const catTemp = document.createElement('p');
+    catTemp.textContent = `<strong>Temperament: </strong>${response.temperament}`;
+    refs.catInfo.append(catName, catDescription, catTemp);
+  });
+
+  fetchImageByBreed(event.target.value).then(response => {
+    const catPicture = document.createElement(`img`);
+    catPicture.src = response[0]?.url;
+    catPicture.width = 480;
+    refs.catInfo.prepend(catPicture);
+  });
+
+})
+
+
+
+
+
