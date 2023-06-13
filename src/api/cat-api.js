092d002aa2d93142283140052169bbe5820dc95d
AlexanderPetrik <painkiller408@gmail.com>
2023-06-14 01:04:02 +0300

    finished

--- src/api/cat-api.js
+++ src/api/cat-api.js
@@ -1,37 +1,41 @@
 const URL = 'https://api.thecatapi.com/v1';
 const headers = {
-    headers: {
-       'x-api-key' : 'api_key=live_tzUAV1WarGXq6gMm0liTGEEZrUB73EAL2S74HlhzvxKlrY4zPEga8ttGvvkkhmxG'
+  headers: {
+      'x-api-key' : 'api_key=live_tzUAV1WarGXq6gMm0liTGEEZrUB73EAL2S74HlhzvxKlrY4zPEga8ttGvvkkhmxG'
     }
-  }
+}
+
+function throwError() {
+  throw new Error('Oops! Something went wrong! Try reloading the page!');
+}
 
 export function fetchImageByBreed(breedId) {
   return fetch(`${URL}/images/search?breed_ids=${breedId}`, headers)
     .then(response => {
       if (!response.ok) {
-        throw new Error('Oops! Something went wrong! Try reloading the page!');
+        throwError();
       }
       return response.json();
-    });
+    }).catch(throwError);
 }
 
 export function fetchCatByBreed(breedId) {
   return fetch(`${URL}/breeds/${breedId}`, headers)
     .then(response => {
       if (!response.ok) {
-        throw new Error('Oops! Something went wrong! Try reloading the page!');
+        throwError();
       }
       return response.json();
-    });
+    }).catch(throwError);
 }
 
 export function fetchBreeds() {
   return fetch(`${URL}/breeds`, headers)
     .then(response => {
       if (!response.ok) {
-        throw new Error('Oops! Something went wrong! Try reloading the page!');
+        throwError();
       }
       return response.json();
-    });
+    }).catch(throwError);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import 'slim-select/dist/slimselect.css';
+import './css/loader.css';
 import './css/styles.css';
 import { fetchCatByBreed, fetchImageByBreed, fetchBreeds } from './api/cat-api';
 import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+
 
 // Получить рефс +
 // Получить список пород +
@@ -8,53 +12,94 @@ import Notiflix from 'notiflix';
 // При выборе из силекта получить полную ин-фу о породе +
 // Вывисти ин-фу на страницу +
 // Очистка +
-// При загрузке показывать загрузку -
-// Добавить обработку ошибок -
+// При загрузке показывать загрузку +
+// Добавить обработку ошибок +
 // Добавить оформление стилей на страницу:
-    // селект -
-    // loader -
-    // Error -
+    // селект +
+    // loader +
+    // Error +
 
 const refs = {
   breedCats: document.querySelector('.breed-select'),
   catInfo: document.querySelector('.cat-info'),
-  loader: document.querySelector('.loader'),
+  loader: document.querySelector('.lds-roller'),
+  error: document.querySelector('.error'),
 }
 
-// console.log(refs);
+const toggleLoader = (show) => {
+  if (show) {
+    refs.loader.classList.remove('hidden-js');
+    return
+  }
+  refs.loader.classList.add('hidden-js');
+};
 
-fetchBreeds().then(response => {
-  console.log(response);
-  const options = response.map((cat) => {
-    const option = document.createElement(`option`);
-    option.value = cat.id;
-    option.textContent = cat.name;
-    return option
+const showError = () => {
+  Notiflix.Notify.failure(
+    "Oops! Something went wrong! Try reloading the page!",
+    {
+      timeout: 6000,
+    },
+  );
+};
+
+const drawInfo = response => {
+  const catName = document.createElement(`h2`);
+  catName.textContent = response.name;
+  const catDescription = document.createElement(`p`);
+  catDescription.textContent = response.description;
+  const catTemp = document.createElement('p');
+  catTemp.innerHTML = `<strong>Temperament: </strong>${response.temperament}`;
+  refs.catInfo.append(catName, catDescription, catTemp);
+};
+
+const drawImage = response => {
+  const catPicture = document.createElement(`img`);
+  catPicture.src = response[0]?.url;
+  catPicture.width = 480;
+  refs.catInfo.prepend(catPicture);
+};
+
+
+
+fetchBreeds()
+  .then(response => {
+    const options = response.map((cat) => {
+      const option = document.createElement(`option`);
+      option.value = cat.id;
+      option.textContent = cat.name;
+      return option
+    })
+    refs.breedCats.append(...options);
+    new SlimSelect({
+      select: '.breed-select'
+    })
   })
-  refs.breedCats.append(...options);
-});
+  .catch(showError)
+  .finally(() => {
+    toggleLoader(false);
+  });
+
+
 
 refs.breedCats.addEventListener('change', (event) => {
+
   refs.catInfo.innerHTML = '';
-
-  fetchCatByBreed(event.target.value).then(response => {
-    const catName = document.createElement(`h2`);
-    catName.textContent = response.name;
-    const catDescription = document.createElement(`p`);
-    catDescription.textContent = response.description;
-    const catTemp = document.createElement('p');
-    catTemp.textContent = `<strong>Temperament: </strong>${response.temperament}`;
-    refs.catInfo.append(catName, catDescription, catTemp);
-  });
-
-  fetchImageByBreed(event.target.value).then(response => {
-    const catPicture = document.createElement(`img`);
-    catPicture.src = response[0]?.url;
-    catPicture.width = 480;
-    refs.catInfo.prepend(catPicture);
-  });
-
-})
+  toggleLoader(true);
+  Promise
+    .all([
+      fetchCatByBreed(event.target.value),
+      fetchImageByBreed(event.target.value),
+    ])
+    .then(response => {
+      drawInfo(response[0]);
+      drawImage(response[1]);
+    })
+    .catch(showError)
+    .finally(() => {
+      toggleLoader(false);
+    })
+});
 
 
 
